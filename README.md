12440cbafa9a6c581a941ad985adcb50f6732578
Amos TINA NFON <amostinanfon17@yahoo.com>
2020-02-10 08:47:10 +0100

    Hi tihs is my first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Here is a react real app 
+
+Build by using all javascript stuff
+
+This app is use from scratch !!!
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App'; 
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
+
+serviceWorker.unregister();
